89c9b15f20e9dcaa7dc67ba0f5951ef349241676
Vadim Mokrushin <mokr.vo@yandex.ru>
2022-11-09 12:06:24 +0700

    Home Work 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let val of Object.values(salaries)) {
+    if (!isNaN(val) && isFinite(val)) {
+      sum += val;
+    }
+  }
+  return sum;
 }
