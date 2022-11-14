276701502d8e72f8e962717302f02f6208aaec3b
Vadim Mokrushin <mokr.vo@yandex.ru>
2022-11-14 13:25:41 +0700

    Решение задач

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    .split("-")
+    .map((item, index) => {
+      if (!item) return;
+      if (!index && str[0] != "-") return item;
+      return item[0].toUpperCase() + item.slice(1);
+    })
+    .join("");
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,7 @@
 function getMinMax(str) {
-  // ваш код...
+  const arr = str
+    .split(" ")
+    .filter((item) => isFinite(item))
+    .map((item) => +item);
+  return { min: Math.min(...arr), max: Math.max(...arr) };
 }
