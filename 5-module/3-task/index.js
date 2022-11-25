202a3f4b747eafc0aab71a18c591e2f23efffe1b
Vadim Mokrushin <mokr.vo@yandex.ru>
2022-11-25 18:51:22 +0700

    Финал

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,13 +1,17 @@
 function initCarousel() {
   const carousel = document.querySelector(".carousel");
-  const arrowLeft = document.querySelector(".carousel__arrow_left");
-  const arrowRight = document.querySelector(".carousel__arrow_right");
-  const inner = document.querySelector(".carousel__inner");
+
+  carousel.addEventListener("click", (event) => {
+    event.target.closest(".carousel__arrow_left") && view.switchLeft();
+    event.target.closest(".carousel__arrow_right") && view.switchRight();
+  });
 
   const view = {
     position: 0,
-    // inner: document.querySelector(".carousel__inner");
-    length: inner.children.length,
+    inner: document.querySelector(".carousel__inner"),
+    length: document.querySelector(".carousel__inner").children.length,
+    arrowLeft: document.querySelector(".carousel__arrow_left"),
+    arrowRight: document.querySelector(".carousel__arrow_right"),
 
     switchLeft() {
       this.position--;
@@ -20,23 +24,18 @@ function initCarousel() {
     },
 
     update() {
-      inner.style.transform = `translateX(-${
-        this.position * inner.offsetWidth
+      this.inner.style.transform = `translateX(-${
+        this.position * this.inner.offsetWidth
       }px)`;
 
       if (this.position > 0 && this.position < this.length) {
-        arrowLeft.style.display = "block";
-        arrowRight.style.display = "block";
+        this.arrowLeft.style.display = "block";
+        this.arrowRight.style.display = "block";
       }
-      if (this.position === 0) arrowLeft.style.display = "none";
-      if (this.position === this.length - 1) arrowRight.style.display = "none";
+      if (this.position === 0) this.arrowLeft.style.display = "none";
+      if (this.position === this.length - 1)
+        this.arrowRight.style.display = "none";
     },
   };
-
   view.update();
-
-  carousel.addEventListener("click", (event) => {
-    event.target.closest(".carousel__arrow_left") && view.switchLeft();
-    event.target.closest(".carousel__arrow_right") && view.switchRight();
-  });
 }
