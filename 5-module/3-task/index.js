3b836718a44497be01ddd1f27453b9a046d54f75
Vadim Mokrushin <mokr.vo@yandex.ru>
2022-11-25 18:36:59 +0700

    Версия 1

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".carousel");
+  const arrowLeft = document.querySelector(".carousel__arrow_left");
+  const arrowRight = document.querySelector(".carousel__arrow_right");
+  const inner = document.querySelector(".carousel__inner");
+
+  const view = {
+    position: 0,
+    // inner: document.querySelector(".carousel__inner");
+    length: inner.children.length,
+
+    switchLeft() {
+      this.position--;
+      this.update();
+    },
+
+    switchRight() {
+      this.position++;
+      this.update();
+    },
+
+    update() {
+      inner.style.transform = `translateX(-${
+        this.position * inner.offsetWidth
+      }px)`;
+
+      if (this.position > 0 && this.position < this.length) {
+        arrowLeft.style.display = "block";
+        arrowRight.style.display = "block";
+      }
+      if (this.position === 0) arrowLeft.style.display = "none";
+      if (this.position === this.length - 1) arrowRight.style.display = "none";
+    },
+  };
+
+  view.update();
+
+  carousel.addEventListener("click", (event) => {
+    event.target.closest(".carousel__arrow_left") && view.switchLeft();
+    event.target.closest(".carousel__arrow_right") && view.switchRight();
+  });
 }
