5036eef07839967b3034b51d711919fc4925dbe8
Vadim Mokrushin <mokr.vo@yandex.ru>
2022-12-13 13:28:31 +0700

    Исправление

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,5 +1,7 @@
 export default function promiseClick(button) {
   return new Promise((resolve) => {
-    button.addEventListener("click", (event) => resolve(event));
+    button.addEventListener("click",
+      (event) => resolve(event)
+    );
   });
 }
